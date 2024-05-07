91e8eccc9c99addf2fd6e819a6d45c1bc82746af
JBKIM <drv98@naver.com>
2024-05-07 11:49:39 +0900

    5월 7일 타임리프 문법

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -13,5 +17,11 @@ public class SampleController {
     public void hello(Model model) {
         log.info("hello");
         model.addAttribute("msg", "Hello World");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("AAA","BBB","CCC","DDD");
+        model.addAttribute("list", list);
     }
 }
